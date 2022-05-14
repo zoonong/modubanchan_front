7faf98687384a8ac5683657e61eccb07b8681800
manyyeon <udayeon@naver.com>
2022-05-15 04:34:47 +0900

    SignUp css 기본 틀 만들기

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -1,20 +1,37 @@
+import { createNextState } from "@reduxjs/toolkit";
 import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
+import styles from "../styles/SignUp/SignUp.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 const SignUp = () => {
   return (
-    <div className="signup">
-      <p>SignUp</p>
-      <p>아이디</p>
-      <input name="ID" type="email" placeholder="아이디" required />
-      <p>비밀번호</p>
-      <input name="password" type="password" placeholder="비밀번호" required />
-      <p>닉네임</p>
-      <input name="nickname" type="text" placeholder="닉네임" required />
-      <p> </p>
+    <div className={cx("SignUp")}>
+      <div className={cx("Title")}>SignUp</div>
+      <div className={cx("Form")}>
+        <div className={cx("Item")}>
+          <span className={cx("Text")}>아이디</span>
+          <input className={cx("Input")} name="ID" type="email" placeholder="아이디" required />
+        </div>
+        <div className={cx("Item")}>
+          <span className={cx("Text")}>비밀번호</span>
+          <input className={cx("Input")} name="password" type="password" placeholder="비밀번호" required />
+        </div>
+        <div className={cx("Item")}>
+          <span className={cx("Text")}>비밀번호 확인</span>
+          <input className={cx("Input")} name="password" type="password" placeholder="비밀번호" required />
+        </div>
+        <div className={cx("Item")}>
+        <span className={cx("Text")}>닉네임</span>
+        <input className={cx("Input")} name="nickname" type="text" placeholder="닉네임" required />
+        </div>
+      </div>
+
       <Link to="/">
-        <button>가입하기</button>
+        <button className={cx("Submit")}>가입하기</button>
       </Link>
     </div>
   );
