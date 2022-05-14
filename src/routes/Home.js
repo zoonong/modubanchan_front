8dae388341375c1985b75cb93f2afdeead6b2ae9
manyyeon <udayeon@naver.com>
2022-05-15 00:09:09 +0900

    user 추가 바로 바로 반영되게 바꿈

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -25,28 +25,59 @@ const UserList = React.memo(function UserList({users}) {
 
 const Home = () => {
   const nextId = useRef(1);
-  const [user, setUser] = useState({
-    id: nextId.current,
-    nickname: ""
+  const [inputs, setInputs] = useState({
+    nickname: "",
+    password: ""
   });
+  const { nickname, password } = inputs;
   const users = useSelector(state => state.users);
   const dispatch = useDispatch();
 
   const onCreateUser = user => dispatch(addUser(user));
-  
+
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const onSubmit = () => {
+    const user = {
+      id: nextId.current,
+      nickname: nickname,
+      password: password
+    };
+    onCreateUser(user);
+
+    setInputs({
+      nickname: "",
+      password: ""
+    });
+    nextId.current += 1;
+  };
+
   return (
     <div>
       <div>Home</div>
+      <input
+          name="nickname"
+          type="text"
+          placeholder="닉네임"
+          onChange={onChange}
+          value={nickname}
+        />
+        <input
+          name="password"
+          type="text"
+          placeholder="비밀번호"
+          onChange={onChange}
+          value={password}
+        />
+        <button type="text" onClick={onSubmit}>사용자 추가</button>
       <div>사용자 리스트</div>
       <UserList users={users} />
-      <button type="text" onClick={() => {
-        setUser({
-          ...user,
-          id: nextId.current
-        });
-        onCreateUser(user);
-        nextId.current += 1
-      }}>사용자 추가</button>
     </div>
   );
 };
