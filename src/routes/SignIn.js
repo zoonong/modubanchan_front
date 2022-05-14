745962064f023ef257c9f7feaa823eaea497ab51
manyyeon <udayeon@naver.com>
2022-05-15 02:55:54 +0900

    sign in 페이지 css 틀 잡기

--- src/routes/SignIn.js
+++ src/routes/SignIn.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
+import styles from "../styles/SignIn/SignIn.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 const SignIn = () => {
   return (
-    <div className="signin">
-      <p>SignIn</p>
-      <p>로고</p>
-      <input name="ID" type="email" placeholder="아이디" required />
-      <p></p>
-      <input name="password" type="password" placeholder="비밀번호" required />
-      <p></p>
-      <button>로그인</button>
-      <p>회원이 아니신가요? </p>
-      <Link to="/SignUp">
-        <p>회원가입</p>
-      </Link>
+    <div className={cx("SignIn")}>
+      <div className={cx("Title")}>Sign In</div>
+      <p className={cx("Logo")}>모두의 반찬</p>
+
+      <div className={cx("Form")}>
+        <input className={cx("Input")} name="ID" type="email" placeholder="아이디" required />
+        <input className={cx("Input")} name="password" type="password" placeholder="비밀번호" required />
+        <button className={cx("Submit")}>로그인</button>
+      </div>
+      
+      <p className={cx("Text")}>
+        <span>회원이 아니신가요?</span>
+        <Link to="/SignUp">
+          <span className={cx("Toggle")}>회원가입</span>
+        </Link>
+      </p>
     </div>
   );
 };
