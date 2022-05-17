c850914a05399483e6b1f3eb4e0080bb9529ac5c
JZU0 <ljzh1030@gmail.com>
2022-05-17 18:15:10 +0900

    부트스트랩 확인

--- src/routes/Category.js
+++ src/routes/Category.js
@@ -1,7 +1,51 @@
 import React from "react";
 
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <>
+      <div>Category</div>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+    </>
+  );
 };
 
 export default Category;
