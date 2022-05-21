9cd0377683331c80a731ffe7c74b498097936093
manyyeon <udayeon@naver.com>
2022-05-21 23:59:41 +0900

    Cart

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -3,7 +3,23 @@ import React from "react";
 const Cart = () => {
   return (
     <div>
-      CART
+      <span>장바구니</span>
+      <div>
+        <button>전체 선택</button>
+        <div>
+          <span>상품 금액</span>
+          <span>배송비</span>
+          <span>결제 예정 금액</span>
+        </div>
+        <div>
+          <span>15000원</span>
+          <span> + </span>
+          <span>5000원</span>
+          <span> = </span>
+          <span>20000원</span>
+        </div>
+        <span>주문하기</span>
+      </div>
     </div>
   );
 };
