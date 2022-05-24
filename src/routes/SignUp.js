00f01df35edd34303fc759ad5f2fafad6d007126
manyyeon <udayeon@naver.com>
2022-05-25 04:29:20 +0900

    Cart.js에 있는 회원가입 코드 옮겨놓음

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import "../App.css";
 import styles from "../styles/SignUp/SignUp.module.scss";
 import classNames from "classnames/bind";
+import axios from "axios";
 
 const cx = classNames.bind(styles);
 
@@ -14,7 +15,7 @@ const UserItem = React.memo(function UserItem({user}) {
   return (
     <ul>
       <li>{user.id}</li>
-      <li>{user.nickname}</li>
+      <li>{user.email}</li>
       <li>{user.password}</li>
     </ul>
   );
@@ -33,35 +34,53 @@ const UserList = React.memo(function UserList({users}) {
 const SignUp = () => {
   const nextId = useRef(1);
   const [inputs, setInputs] = useState({
-    nickname: "",
-    password: ""
+    email: "",
+    password1: "",
+    password2: ""
   });
-  const { nickname, password } = inputs;
   const users = useSelector(state => state.users);
   const dispatch = useDispatch();
 
   const onCreateUser = user => dispatch(addUser(user));
 
-  const onChange = e => {
-    const {name, value} = e.target;
+  function userAccounts() {
+    axios.post("http://127.0.0.1:8000/accounts/", {
+      email: inputs.email,
+      password1: inputs.password1,
+      password2: inputs.password2
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+    }); 
+  }
+
+  const onChange = (e) => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
     setInputs({
-      ...inputs,
-      [name]: value
+      ...inputs, // 기존의 user 객체를 복사한 뒤
+      [name]: value // name 키를 가진 값을 value 로 설정
     });
   };
+  
 
   const onSubmit = () => {
     const user = {
       id: nextId.current,
-      nickname: nickname,
-      password: password,
+      email: inputs.email,
+      password: inputs.password1,
       profilePicture: require("../images/profile.jpeg")
     };
+    userAccounts();
     onCreateUser(user);
 
     setInputs({
-      nickname: "",
-      password: ""
+      ...inputs,
+      email: "",
+      password1: "",
+      password2: ""
     });
     nextId.current += 1;
   };
@@ -70,20 +89,16 @@ const SignUp = () => {
       <div className={cx("Title")}>SignUp</div>
       <div className={cx("Form")}>
         <div className={cx("Item")}>
-          <span className={cx("Text")}>아이디</span>
-          <input className={cx("Input")} name="ID" type="email" placeholder="아이디" required />
+          <span className={cx("Text")}>이메일</span>
+          <input className={cx("Input")} name="email" type="email" placeholder="이메일" onChange={onChange} value={inputs.email} required />
         </div>
         <div className={cx("Item")}>
           <span className={cx("Text")}>비밀번호</span>
-          <input className={cx("Input")} name="password" type="password" placeholder="비밀번호" onChange={onChange} value={password} required />
+          <input className={cx("Input")} name="password1" type="password" placeholder="비밀번호" onChange={onChange} value={inputs.password1} required />
         </div>
         <div className={cx("Item")}>
           <span className={cx("Text")}>비밀번호 확인</span>
-          <input className={cx("Input")} name="password" type="password" placeholder="비밀번호 확인" required />
-        </div>
-        <div className={cx("Item")}>
-        <span className={cx("Text")}>닉네임</span>
-        <input className={cx("Input")} name="nickname" type="text" placeholder="닉네임" onChange={onChange} value={nickname} required />
+          <input className={cx("Input")} name="password2" type="password" placeholder="비밀번호 확인" onChange={onChange} value={inputs.password2} required />
         </div>
       </div>
 
