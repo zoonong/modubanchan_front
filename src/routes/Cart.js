3f94291c5ac55e04473a2e6c75f5825d55610d7d
manyyeon <udayeon@naver.com>
2022-05-29 19:37:27 +0900

    장바구니 이동 modal

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,10 +1,31 @@
 import { React, useState, useDispatch } from "react";
 import CartProduct from "../components/CartProduct";
 import axios from 'axios';
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import styles from "../styles/Cart/Cart.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 const Cart = () => {
   return (
-    <div>    
+    <div className={cx("Cart")}>
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>장바구니로 이동하시겠습니까?</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p></p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary">No. Continue Shopping</Button>
+          <Button variant="primary">Yes. Go to Cart</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+
       <span>장바구니</span>
       <CartProduct />
       <CartProduct />
